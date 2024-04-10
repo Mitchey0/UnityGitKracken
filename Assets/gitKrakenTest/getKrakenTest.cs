0434b91e0c9b49cba652c8a80c40b5df81e8d825
MichelleO <Ortizmich87@gmail.com>
2024-04-10 15:32:38 -0500

    Additional Changes
    Added more walls and player to scene.

--- Assets/gitKrakenTest/getKrakenTest.cs
+++ Assets/gitKrakenTest/getKrakenTest.cs
@@ -7,12 +7,12 @@ public class getKrakenTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Code goes here
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Left, right, right, left
     }
 }
